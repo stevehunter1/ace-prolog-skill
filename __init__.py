5398842bb3ff7312f8b4616f6764adbca4f71ba4
Felix Frank <post@felix-frank.com>
2019-06-06 19:13:53 +0200

    some things

--- __init__.py
+++ __init__.py
@@ -5,9 +5,10 @@ class AceProlog(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('prolog.ace.intent')
+    @intent_file_handler('remember.intent')
     def handle_prolog_ace(self, message):
-        self.speak_dialog('prolog.ace')
+        text = message.data.get('text')
+        self.speak_dialog('answer', data={'result': text})
 
 
 def create_skill():
