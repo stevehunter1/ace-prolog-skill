5a866bf371a9bdfe7ae64f46937ab25bbf47dc7f
Felix Frank <post@felix-frank.com>
2019-06-11 18:01:23 +0200

    request

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, FileSystemAccess
+from mycroft.util.log import LOG
 
 import requests
 
@@ -7,6 +8,25 @@ def get_test(s):
     r = requests.get("https://www.felix-frank.com/")
     return r.text
 
+def load_data_file(self, filename, mode="r"):
+    file_system = FileSystemAccess(str(self.skill_id))
+    file = file_system.open(filename, mode)
+    data = file.read()
+    file.close()
+    return data
+
+def save_data_file(self, filename, data, mode="w"):
+    try:
+        file_system = FileSystemAccess(str(self.skill_id))
+        file = file_system.open(filename, mode)
+        file.write(data)
+        file.close()
+        return True
+    except Exception as e:
+        LOG.warning("could not save skill file " + filename)
+        LOG.error(e)
+        return False
+
 class AceProlog(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
@@ -14,6 +34,7 @@ class AceProlog(MycroftSkill):
     @intent_file_handler('remember.intent')
     def handle_remember(self, message):
         remember = message.data.get('text')
+        save_data_file(self, "test.txt", "hallo")
         self.speak_dialog('answer', data={'result': remember})
 
     @intent_file_handler('prove.intent')
