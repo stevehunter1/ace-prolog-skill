d3041d4042e4445dd4cd2d951849d0b1d47b317e
Felix Frank <post@felix-frank.com>
2019-06-11 15:32:43 +0200

    zeug

--- __init__.py
+++ __init__.py
@@ -6,9 +6,14 @@ class AceProlog(MycroftSkill):
         MycroftSkill.__init__(self)
 
     @intent_file_handler('remember.intent')
-    def handle_prolog_ace(self, message):
+    def handle_remember(self, message):
         text = message.data.get('text')
         self.speak_dialog('answer', data={'result': text})
+
+    @intent_file_handler('prove.intent')
+    def handle_prove(self, message):
+        prove = message.data.get('text')
+        self.speak_dialog('answer', data={'result':prove})
 
 
 def create_skill():
